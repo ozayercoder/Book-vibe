11edbd5882f40f5fa0e000cbfc2f9bb9da192739
Ozayer-Coder <ozayer212714@gmail.com>
2026-09-20 16:21:44 +0600

    env added

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -12,9 +12,16 @@ interface IBookDetailsPageProps {
 }
 
 const getBooks = async () => {
-  const res = await fetch("http://localhost:3000/booksData.json");
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return [];
+  }
 };
 
 const BookDetailsPage = async ({ params }: IBookDetailsPageProps) => {
